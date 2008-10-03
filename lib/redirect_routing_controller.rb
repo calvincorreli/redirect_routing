a686f79ee62b9ca74529649d047e133c4c9116b2
GuillaumeGentil <guillaumegentil@gmail.com>
2008-10-03 05:05:55 -0700

    added delete :conditions on options

--- lib/redirect_routing_controller.rb
+++ lib/redirect_routing_controller.rb
@@ -1,6 +1,7 @@
 class RedirectRoutingController < ActionController::Base
   def redirect
     options = params[:args].extract_options!
+    options.delete(:conditions)
     status = options.delete(:permanent) == true ? :moved_permanently : :found
     url_options = params[:args].first || options
     redirect_to url_options, :status => status
